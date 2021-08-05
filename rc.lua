a8ff2b8ab140bb6e6df86a771c2b89ad69464d83
alex <alexandruturcanu915@gmail.com>
2021-08-05 10:37:35 +0300

    Restarted

--- rc.lua
+++
@@ -1,7 +0,0 @@
-pcall( require, "luarocks.loader" )
-
-require( "error" ).Init()
-require( 'theme' ).Init()
-require( 'window-manager' ).Init()
-require( 'keybindings' ).Init()
-require('window-manager.signals').Init()
--- variables.lua
+++
@@ -1,9 +0,0 @@
-local r = {}
-
-r.terminal = "alacritty"
-r.editor = os.getenv( "EDITOR" ) or "nvim"
-r.modkey = "Mod4"
-r.editor_cmd = r.terminal .. " -e " .. r.editor
-
-return r
-
