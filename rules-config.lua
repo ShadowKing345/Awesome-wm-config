270dd0b5f9c1854fe8107cf1db4b7ca614401801
alex <alexandruturcanu915@gmail.com>
2021-09-18 10:54:43 +0300

    formatted

--- rules-config.lua
+++ rules-config.lua
@@ -10,17 +10,14 @@ rules.base_properties = {
     raise = true,
     size_hints_honor = false,
     screen = awful.screen.preferred,
-    placement = awful.placement.no_overlap + awful.placement.no_offscreen
+    placement = awful.placement.no_overlap + awful.placement.no_offscreen,
 }
 
 rules.floating_any = {
-    instance = { "DTA", "copyq", },
-    class = {
-        "Arandr", "Gpick", "Kruler", "MessageWin", "Sxiv", "Wpa_gui", "pinentry", "veromix",
-		"xtightvncviewer"
-	},
-	name = { "Event Tester", },
-	role = { "AlarmWindow", "pop-up", }
+    instance = {"DTA", "copyq"},
+    class = {"Arandr", "Gpick", "Kruler", "MessageWin", "Sxiv", "Wpa_gui", "pinentry", "veromix", "xtightvncviewer"},
+    name = {"Event Tester"},
+    role = {"AlarmWindow", "pop-up"},
 }
 
 function rules:init(args)
@@ -30,30 +27,13 @@ function rules:init(args)
 
     -- Build rules
     self.rules = {
-        {
-            rule = {},
-            properties = args.base_properties or self.base_properties
-        },
-        {
-            rule_any = args.floating_any or self.floating_any,
-            properties = { floating = true }
-        },
-        {
-            rule_any = { type = {"normal", "dialog" } },
-            properties = { titlebars_enabled = true }
-        },
-
-        -- discord
-        {
-            rule = { class = "discord" },
-            properties = { tag = "Tag3" }
-        },
+        {rule = {}, properties = args.base_properties or self.base_properties},
+        {rule_any = args.floating_any or self.floating_any, properties = {floating = true}},
+        {rule_any = {type = {"normal", "dialog"}}, properties = {titlebars_enabled = true}}, -- discord
+        {rule = {class = "discord"}, properties = {tag = "Tag3", screen = 1, maximized = true}},
 
         -- attempt to make the "splash screen" for Jetbrain IDE float. Shockingly worked.
-        {
-            rule = { name = "win0" },
-            properties = { floating = true }
-        },
+        {rule = {name = "win0"}, properties = {floating = true}},
     }
 
     awful.rules.rules = rules.rules
