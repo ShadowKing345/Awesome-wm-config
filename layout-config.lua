38d76de598320a86d818292818e37a509a4300c0
alex <alexandruturcanu915@gmail.com>
2021-11-24 23:25:00 +0300

    got rid of variables that will never be used

--- layout-config.lua
+++ layout-config.lua
@@ -1,27 +1,13 @@
 local awful = require("awful")
-local redflat = require("redflat")
-local beautiful = require("beautiful")
-
 local binaryTreeLayout = require("layouts.binary-tree-layout")
-
-local client = client
 
 local layouts = {}
 
 function layouts:init()
-    local layset = {
-      binaryTreeLayout:build({})
-    }
+  local layset = {binaryTreeLayout:build({})}
 
-    awful.layout.layouts = layset
+  awful.layout.layouts = layset
 end
 
--- advance layout settings
-redflat.layout.map.notification = true
-redflat.layout.map.notification_style = {icon = redflat.util.table.check(beautiful, "widget.layoutbox.icon.usermap")}
-
--- layout handler for grid (replace if different handler wanted)
-client.disconnect_signal("request::geometry", awful.layout.move_handler)
-client.connect_signal("request::geometry", redflat.layout.common.mouse.move)
 
 return layouts
