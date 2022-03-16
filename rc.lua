83ea08a8e36ca566494914e7a8046f8c44a467b2
alex <alexandruturcanu915@gmail.com>
2022-03-17 02:18:31 +0300

    formatted

--- rc.lua
+++ rc.lua
@@ -1,32 +1,22 @@
 pcall(require, "luarocks.loader")
 
-local gears = require("gears")
-local awful = require("awful")
-require("awful.autofocus")
-local beautiful = require("beautiful")
-require("awful.hotkeys_popup.keys")
+local awful = require "awful"
+local beautiful = require "beautiful"
+require "awful.autofocus"
+require "awful.hotkeys_popup.keys"
+require "error-config"
 
-require("error-config")
+local env = require "env-config"{}
+beautiful.init(env.themePath)
+awful.layout.layouts = { awful.layout.suit.floating }
 
-beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
-
-local env = require("env-config")()
-
-awful.layout.layouts = {awful.layout.suit.floating}
-
-local screenConfig = require("screen-config")(env)
+local screenConfig = require "screen-config"(env)
 awful.screen.connect_for_each_screen(screenConfig.init)
 
-local bindingConfig = require("binding-config")(env)
+local bindingConfig = require "binding-config"(env)
 root.keys(bindingConfig.keys.global)
 root.buttons(bindingConfig.mouse.global)
 
-require("rules-config"):init{hotkeys = {keys = bindingConfig.keys, mouse = bindingConfig.mouse}}
-
+require("rules-config"):init { hotkeys = { keys = bindingConfig.keys, mouse = bindingConfig.mouse } }
 require("signals").init(env)
-
-
-if not gears.filesystem.file_readable("/tmp/awesomeWmFirstBoot") then
-  require("autostart-config").run()
-  awful.spawn.with_shell("touch /tmp/awesomeWmFirstBoot")
-end
+require("autostart-config").run()
