cb5da8b06aa710ff2b2420cdbf45d7ea41956fd7
alex <alexandruturcanu915@gmail.com>
2022-03-17 02:18:47 +0300

    formatted

--- signals.lua
+++ signals.lua
@@ -1,24 +1,24 @@
-local awful = require("awful")
-local beautiful = require("beautiful")
+local awful = require "awful"
+local beautiful = require "beautiful"
 
-local signals = {mt ={}}
+local signals = { mt = {} }
 
 function signals:init(env)
-  env = env or {}
+    env = env or {}
 
-client.connect_signal("manage", function (c)
-  if awesome.startup and not c.size_hints.user_position and not c.size_hints.program_position then
-    awful.placement.no_offscreen(c)
-  end
-end)
+    client.connect_signal("manage", function(c)
+        if awesome.startup and not c.size_hints.user_position and not c.size_hints.program_position then
+            awful.placement.no_offscreen(c)
+        end
+    end)
 
-client.connect_signal("focus", function (client)
-  client.border_color = beautiful.border_focus
-end)
+    client.connect_signal("focus", function(client)
+        client.border_color = beautiful.border_focus
+    end)
 
-client.connect_signal("unfocus", function (client)
-  client.border_color = beautiful.border_normal
-end)
+    client.connect_signal("unfocus", function(client)
+        client.border_color = beautiful.border_normal
+    end)
 end
 
 return setmetatable(signals, signals.mt)
