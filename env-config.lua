796838b01fc3e2cc7ac3aa37d44c2e089b4dc6d1
alex <alexandruturcanu915@gmail.com>
2022-03-17 02:18:16 +0300

    Added emmylua tags.

--- env-config.lua
+++ env-config.lua
@@ -1,17 +1,42 @@
-local env = {mt = {}}
+---@module'gears'
+local gears = require "gears"
 
-function env:new()
-  self.terminal = "kitty"
-  self.editor = os.getenv("EDITOR") or "nvim"
-  self.editorCmd = self.terminal .. "-e" .. self.editor
+---@class EnvConfig
+---@field terminal string
+---@field editor string
+---@field editorCmd string
+---@field modKey string
+---@field themePath string
+local env = {metatable = {}}
+env.metatable.__index = env
 
-  self.modKey = "Mod4"
+---@class EnvArgs
+---@field terminal? string #Name of the terminal application.
+---@field editor? string #Name of editor.
+---@field modKey? string #Super modifier to be used.
+---@field themePath? string #Path to theme.lua file.
 
-  return self
+---@param args? EnvArgs #Optional arguments for environment configuration.
+---@return EnvConfig
+function env:new(args)
+  args = args or {}
+  self = {}
+    self.terminal = args.terminal or "kitty"
+    self.editor = args.editor or os.getenv "EDITOR" or "nvim"
+    self.editorCmd = self.terminal .. "-e" .. self.editor
+
+    self.modKey = args.modKey or "Mod4"
+
+    self.themePath = args.themePath or gears.filesystem.get_xdg_config_home() .. "awesome/" .. "theme/theme.lua"
+
+    return setmetatable(self, self.metatable)
 end
 
-function env.mt:__call()
-  return env:new()
+---@param args? EnvArgs #Optional arguments for environment configuration.
+---@return EnvConfig
+function env.metatable:__call(args)
+  return env:new(args)
 end
 
-return setmetatable(env, env.mt)
+---@type EnvConfig
+return setmetatable(env, env.metatable)
