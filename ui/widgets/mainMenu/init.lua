f87f860857de5df0ec1a8e19ef128b54ff62277b
alex <alexandruturcanu915@gmail.com>
2022-04-07 00:07:58 +0300

    Added rendering categories and application icons.

--- ui/widgets/mainMenu/init.lua
+++ ui/widgets/mainMenu/init.lua
@@ -8,15 +8,33 @@ local setmetatable = setmetatable
 local awful = require "awful"
 local beautiful = require "beautiful"
 local capi = { screen = screen, mouse = mouse, client = client }
+local gfs = require "gears.filesystem"
 local gTable = require "gears.table"
+local gShape = require "gears.shape"
+local menu_gen = require "menubar.menu_gen"
 local wibox = require "wibox"
 
-local button = require((...):match ".*" .. ".button")
+local relPath = (...):match ".*"
+local application = require(relPath .. ".application")
+local category = require(relPath .. ".category")
+local button = require(relPath .. ".button")
 local inputField = require "ui.widgets.inputField"
 local utils = require "utils"
 
 --------------------------------------------------
-local mainMenu = { widget = nil, mt = {} }
+local mainMenu = { applications = {}, widget = nil, mt = {} }
+
+local vSeperator = wibox.widget {
+    orientation = "vertical",
+    forced_width = 1,
+    widget = wibox.widget.separator,
+}
+
+local hSeperator = wibox.widget {
+    orientation = "horizontal",
+    forced_height = 1,
+    widget = wibox.widget.separator,
+}
 
 function mainMenu.default_style()
     return {
@@ -48,23 +66,29 @@ function mainMenu:init(args)
 
     self.widget = wibox {
         ontop = true,
-    }
-
-    self.widget:geometry {
         width = 720,
         height = 550,
     }
 
-    local vSeperator = wibox.widget {
-        orientation = "vertical",
-        forced_width = 1,
-        widget = wibox.widget.separator,
+    local categories = {
+        category { category = { icon_name = "", name = "All" } },
+        spacing = 1,
+        layout = wibox.layout.flex.vertical,
     }
 
-    local hSeperator = wibox.widget {
-        orientation = "horizontal",
-        forced_height = 1,
-        widget = wibox.widget.separator,
+    for _, v in pairs(menu_gen.all_categories) do
+        local index = 1
+        while index <= #categories and (categories[index].category.name == "All" or categories[index].category.name < v.name) do
+            index = index + 1
+        end
+        table.insert(categories, index, category { category = v })
+    end
+
+    self.widget.applications = wibox.widget {
+        spacing = 10,
+        forced_num_cols = 5,
+        horizontal_expand = true,
+        layout = wibox.layout.grid.vertical,
     }
 
     self.widget:setup {
@@ -97,6 +121,7 @@ function mainMenu:init(args)
                 {
                     hSeperator,
                     {
+                        categories,
                         bg = args.style.bg_left,
                         widget = wibox.container.background,
                     },
@@ -124,7 +149,7 @@ function mainMenu:init(args)
                                     utils.aButton {
                                         modifiers = {},
                                         button = 1,
-                                        callback = awesome.quit,
+                                        callback = function() awesome.quit() end,
                                     }
                                 }
                             },
@@ -148,7 +173,18 @@ function mainMenu:init(args)
                 {
                     {
                         inputField {
-                            placeholder = "Search...",
+                            prompt_args = {
+                                prompt = "Search: ",
+                                completion_callback = awful.completion.shell,
+                                history_path = gfs.get_cache_dir() .. "/history_menu",
+                                done_callback = function()
+                                    mainMenu:hide()
+                                end,
+                                change_callback = function(query)
+                                    require "naughty".notify { text = tostring(query) }
+
+                                end
+                            }
                         },
                         margins = 12,
                         widget = wibox.container.margin,
@@ -160,6 +196,29 @@ function mainMenu:init(args)
                 {
                     hSeperator,
                     {
+                        {
+                            {
+                                {
+                                    self.widget.applications,
+                                    margins = 10,
+                                    widget = wibox.container.margin,
+                                },
+                                widget = wibox.container.background,
+                            },
+                            {
+                                {
+                                    bar_color = "#ff0000",
+                                    handle_color = "#ffff00",
+                                    handle_width = 100,
+                                    value = 25,
+                                    widget = wibox.widget.slider,
+                                },
+                                direction = "west",
+                                widget = wibox.container.rotate,
+                            },
+                            expand = "outer",
+                            layout = wibox.layout.align.horizontal,
+                        },
                         bg = args.style.bg_right,
                         widget = wibox.container.background,
                     },
@@ -236,8 +295,15 @@ function mainMenu:show(args)
     local coords = args.coords or nil
     local s = args.screen or capi.mouse.screen
 
-    self:set_coords(s, coords)
+    self.widget.applications:reset()
+    for _, v in ipairs(self.applications) do
+        self.widget.applications:add(wibox.widget {
+            application { application = v },
+            widget = wibox.container.place,
+        })
+    end
 
+    self:set_coords(s, coords)
     self.widget.visible = true
 end
 
@@ -252,6 +318,13 @@ end
 function mainMenu:new(args)
     if not self.widget then
         self:init(args)
+    end
+
+    if #self.applications < 1 then
+        menu_gen.generate(function(result)
+            self.applications = result
+            table.sort(self.applications, function(a, b) return a.name < b.name end)
+        end)
     end
 
     return self
