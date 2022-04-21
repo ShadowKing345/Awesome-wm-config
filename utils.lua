a45ae31f2f6b193b97ffd18c36955b829de8b23e
alex <alexandruturcanu915@gmail.com>
2022-04-22 01:07:07 +0300

    Fixed, Improved table to json converter.

--- utils.lua
+++ utils.lua
@@ -3,41 +3,75 @@
 --      A collection of utility function used throught the project.
 --
 --------------------------------------------------
-local math = math
+local math  = math
 local pairs = pairs
-local type = type
-local unpack = unpack or table.unpack
+local type  = type
 
 local aButton = require "awful.button"
-local aKey = require "awful.key"
-local gTable = require "gears.table"
+local aKey    = require "awful.key"
 
 --------------------------------------------------
 local utils = { button_names = aButton.names }
 
 ---Creates a pretty JSON string from an object recursively.
----*NOTE: Lua tables with array values will have the index of the value be printed as the raw number.
----Eg. {"test"} = {1: "test"}*
+---*Note: All array objects enter a $Array Field instead due to how lua combines dictionarys and arrays.*
 ---@param tbl table #Table to be turned into a JSON object.
----@param opt TblToJsonOptions #Settings for the parser.
+---@param depth number #How far in we are.
+---@param indent number #Sets the indent from the left. Use to offset the text.
+---@param pretty boolean #If true indents and new lines will be applied. Default: true
 ---@return string
-function utils.tblToJson(tbl, opt)
-    ---@type TblToJsonOptions
-    local _opt = gTable.clone(opt or { indent = 4, pretty = true })
-    _opt.offset = _opt.indent + (_opt.offset or 0)
+function utils.tblToJson(tbl, pretty, indent, depth)
+    pretty = pretty or false
+    indent = (indent or 2) * (pretty and 1 or 0)
+    depth  = depth or 1
 
-    local newChar = _opt.pretty and "\n" or ""
-    local iString = _opt.pretty and (" "):rep(_opt.offset) or ""
-
-    local result = "{" .. newChar
+    local array, hasItem, keys = {}, false, {}
 
     for k, v in pairs(tbl) do
-        local t = type(v)
-        k = type(k) == "string" and "\"" .. k .. "\"" or k
-        result = ("%s%s%s:%s,%s"):format(result, iString, k, (_opt.pretty and " " or "") .. (t == "table" and utils.tblToJson(v, _opt) or (t == "string" and "\"" .. v .. "\"" or tostring(v))), newChar)
+        local vType = type(v)
+        if type(k) == "number" then
+            if vType == "table" then
+                table.insert(array, utils.tblToJson(v, pretty, indent, depth + 1))
+            elseif vType == "string" then
+                table.insert(array, "\"" .. v .. "\"")
+            else
+                table.insert(array, tostring(v))
+            end
+        else
+            hasItem = true
+            local prefix = "\"" .. tostring(k) .. "\": "
+            if vType == "table" then
+                table.insert(keys, prefix .. utils.tblToJson(v, pretty, indent, depth + 1))
+            elseif vType == "string" then
+                table.insert(keys, prefix .. "\"" .. v .. "\"")
+            else
+                table.insert(keys, prefix .. tostring(v))
+            end
+        end
     end
 
-    return result:sub(1, (_opt.pretty and -3 or -2)) .. newChar .. (_opt.pretty and (" "):rep(math.max(_opt.offset - _opt.indent, 0)) or "") .. "}"
+    local prettyStr = pretty and "\n" or ""
+    local indentStr = (" "):rep(indent * depth)
+    local prefixIndent = (" "):rep(indent * math.max(depth - 1, 0))
+
+    local result = prefixIndent .. "{" .. prettyStr
+
+    if #array > 0 then
+        local arrayIndent = (" "):rep(indent * depth * 2)
+        result = result .. indentStr .. "$Array: [" .. prettyStr
+        result = result .. arrayIndent .. table.concat(array, "," .. prettyStr .. arrayIndent) .. prettyStr
+        result = result .. indentStr .. "]"
+        if hasItem then
+            result = result .. ","
+        end
+        result = result .. prettyStr
+    end
+
+    if #keys > 0 then
+        result = result .. indentStr .. table.concat(keys, "," .. prettyStr .. indentStr) .. prettyStr
+    end
+
+    return result .. prefixIndent .. "}"
 end
 
 ---Clamps a number between two values
@@ -77,8 +111,3 @@ return utils
 ---@field callback? fun():nil #Function called when key is pressed.
 ---@field press? fun():nil #Function called when key is pressed.
 ---@field release? fun():nil #Function called when key is released.
-
----@class TblToJsonOptions #Settings for the parser.
----@field offset number #The offset of indent.
----@field indent number #Sets the indent from the left. Use to offset the text.
----@field pretty boolean #If true indents and new lines will be applied. Default: true
