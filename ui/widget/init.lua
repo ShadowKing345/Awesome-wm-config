dab938b0e6ed252c0e7f8c687fe4a28fe196d64e
alex <alexandruturcanu915@gmail.com>
2022-04-24 10:04:45 +0300

    Added central collection of classes for widgets

---
+++ ui/widget/init.lua
@@ -0,0 +1,17 @@
+--[[
+
+    This will simply containl all the class references used throught the application.
+
+--]]
+--------------------------------------------------
+
+---@class ButtonStyle #Style class for buttons.
+---@field normal string #Normal color or default when nothing is meant to happen.
+---@field hover string #Color for when mouse is over button.
+---@field active string #Color for when the button is pressed.
+
+---@class Cardinal #The cardinal amounts for a objects.
+---@field top any #Top value.
+---@field left any #Left value.
+---@field rigth any #Right value.
+---@field bottom any #Bottom value.
