f67b86dc1dc531404d4286a0ef3b225055a02f43
alex <alexandruturcanu915@gmail.com>
2022-04-25 09:59:08 +0300

    Improved and added state changes

--- ui/widget/taglist.lua
+++ ui/widget/taglist.lua
@@ -1,21 +1,23 @@
---------------------------------------------------
---
---      Custom Taglist
---
+--[[
+
+    Custom Taglist
+
+--]]
 --------------------------------------------------
 local setmetatable = setmetatable
 
-local awful = require "awful"
-local wibox = require "wibox"
+local awful     = require "awful"
+local beautiful = require "beautiful"
+local gTable    = require "gears.table"
+local wibox     = require "wibox"
 
 local aButton = require "utils".aButton
 
 --------------------------------------------------
----@type WidgetModule
-local taglist = { widgets = {}, mt = {} }
+local M = { mt = {} }
 
 ---@param env EnvArgs
-function taglist.default_buttons(env)
+function M.default_buttons(env)
     return {
         aButton {
             modifiers = {},
@@ -50,36 +52,80 @@ function taglist.default_buttons(env)
     }
 end
 
----@return DefaultTemplate
-function taglist.default_template()
+function M.default_template(style)
     return {
         template = {
-            nil,
             {
+                nil,
                 {
-                    id = "text_role",
-                    widget = wibox.widget.textbox,
-                    align = "center",
+                    {
+                        id = "text_role",
+                        widget = wibox.widget.textbox,
+                        align = "center",
+                    },
+                    widget = wibox.container.background,
+                    forced_width = 30
                 },
-                widget = wibox.container.background,
-                forced_width = 30
+                {
+                    id = "background_role",
+                    widget = wibox.container.background,
+                    forced_height = 3,
+                },
+                layout = wibox.layout.align.vertical,
             },
-            {
-                id = "background_role",
-                widget = wibox.container.background,
-                forced_height = 3,
-            },
-            layout = wibox.layout.align.vertical,
+            id              = "bg",
+            bg              = style.bg.normal,
+            widget          = wibox.container.background,
+            create_callback = function(self)
+                local bg = self:get_children_by_id "bg"[1]
+
+                if not bg then return end
+
+                bg._private.bg = style.bg
+                bg._private.fg = style.fg
+
+                function bg:change_state(state)
+                    self.bg = self._private.bg[state]
+                    self.fg = self._private.fg[state]
+                end
+
+                bg:connect_signal("mouse::enter", function() bg:change_state "hover" end)
+                bg:connect_signal("mouse::leave", function() bg:change_state "normal" end)
+                bg:connect_signal("button::press",
+                    function(_, _, _, button)
+                        if button ~= 1 then return end
+                        bg:change_state "active"
+                    end)
+                bg:connect_signal("button::release",
+                    function(_, _, _, button)
+                        if button ~= 1 then return end
+                        bg:change_state "hover"
+                    end)
+            end,
+        },
+    }
+end
+
+function M.default_style()
+    return {
+        bg = {
+            normal = beautiful["taglist_bg_normal"],
+            hover  = beautiful["taglist_bg_hover"],
+            active = beautiful["taglist_bg_active"],
+        },
+        fg = {
+            normal = beautiful["taglist_fg_normal"],
+            hover  = beautiful["taglist_fg_hover"],
+            active = beautiful["taglist_fg_active"],
         }
     }
 end
 
----@param args {buttons:table,screen:table}
-function taglist:new(args)
+function M:new(args)
     args = args or {}
+    args.style = gTable.merge(M.default_style(), args.style or {})
 
-    ---@type DefaultTemplate
-    local template = self.default_template()
+    local template = self.default_template(args.style)
 
     local w = awful.widget.taglist {
         screen = args.screen,
@@ -87,14 +133,15 @@ function taglist:new(args)
         buttons = args.buttons,
         widget_template = template.template
     }
-    table.insert(self.widgets, w)
+
+    gTable.crush(w, M, true)
 
     return w
 end
 
 --------------------------------------------------
-function taglist.mt:__call(...)
-    return taglist:new(...)
+function M.mt:__call(...)
+    return M:new(...)
 end
 
-return setmetatable(taglist, taglist.mt)
+return setmetatable(M, M.mt)
--- ui/widget/tasklist.lua
+++ ui/widget/tasklist.lua
@@ -8,14 +8,15 @@ local setmetatable = setmetatable
 
 local awful     = require "awful"
 local beautiful = require "beautiful"
+local gTable    = require "gears.table"
 local wibox     = require "wibox"
 
 local aButton = require "utils".aButton
 
 --------------------------------------------------
-local tasklist = { widgets = {}, mt = {} }
+local M = { widgets = {}, mt = {} }
 
-function tasklist.default_buttons()
+function M.default_buttons()
     return {
         aButton {
             modifiers = {},
@@ -46,50 +47,98 @@ function tasklist.default_buttons()
     }
 end
 
-function tasklist.default_template()
+function M.default_template(style)
     return {
         layout = {
             layout = wibox.layout.fixed.horizontal
         },
         template = {
-            nil,
             {
+
+                nil,
                 {
                     {
                         {
                             {
-                                id     = "icon_role",
-                                widget = wibox.widget.imagebox,
+                                {
+                                    id     = "icon_role",
+                                    widget = wibox.widget.imagebox,
+                                },
+                                margins = 2,
+                                widget  = wibox.container.margin,
                             },
-                            margins = 2,
-                            widget  = wibox.container.margin,
+                            {
+                                id           = "text_role",
+                                widget       = wibox.widget.textbox,
+                                forced_width = beautiful["tasklist_width"] or 100,
+                            },
+                            layout = wibox.layout.fixed.horizontal,
                         },
-                        {
-                            id           = "text_role",
-                            widget       = wibox.widget.textbox,
-                            forced_width = beautiful["tasklist_width"] or 100,
-                        },
-                        layout = wibox.layout.fixed.horizontal,
+                        left   = 2,
+                        right  = 8,
+                        widget = wibox.container.margin
                     },
-                    left   = 2,
-                    right  = 8,
-                    widget = wibox.container.margin
+                    widget = wibox.container.background,
                 },
-                widget = wibox.container.background,
+                {
+                    id = "background_role",
+                    widget = wibox.container.background,
+                    forced_height = 3,
+                },
+                layout = wibox.layout.align.vertical,
             },
-            {
-                id = "background_role",
-                widget = wibox.container.background,
-                forced_height = 3,
-            },
-            layout = wibox.layout.align.vertical,
+            id              = "bg",
+            bg              = style.bg.normal,
+            widget          = wibox.container.background,
+            create_callback = function(self)
+                local bg = self:get_children_by_id "bg"[1]
+
+                if not bg then return end
+
+                bg._private.bg = style.bg
+                bg._private.fg = style.fg
+
+                function bg:change_state(state)
+                    self.bg = self._private.bg[state]
+                    self.fg = self._private.fg[state]
+                end
+
+                bg:connect_signal("mouse::enter", function() bg:change_state "hover" end)
+                bg:connect_signal("mouse::leave", function() bg:change_state "normal" end)
+                bg:connect_signal("button::press",
+                    function(_, _, _, button)
+                        if button ~= 1 then return end
+                        bg:change_state "active"
+                    end)
+                bg:connect_signal("button::release",
+                    function(_, _, _, button)
+                        if button ~= 1 then return end
+                        bg:change_state "hover"
+                    end)
+            end,
         }
     }
 end
 
-function tasklist:new(args)
+function M.default_style()
+    return {
+        bg = {
+            normal = beautiful["tasklist_bg_normal"],
+            hover  = beautiful["tasklist_bg_hover"],
+            active = beautiful["tasklist_bg_active"],
+        },
+        fg = {
+            normal = beautiful["tasklist_fg_normal"],
+            hover  = beautiful["tasklist_fg_hover"],
+            active = beautiful["tasklist_fg_active"],
+        }
+    }
+end
+
+function M:new(args)
     args = args or {}
-    local template = self.default_template()
+    args.style = gTable.merge(M.default_style(), args.style or {})
+    local template = self.default_template(args.style)
 
     local w = awful.widget.tasklist {
         screen = args.screen,
@@ -104,8 +153,8 @@ function tasklist:new(args)
 end
 
 --------------------------------------------------
-function tasklist.mt:__call(...)
-    return tasklist:new(...)
+function M.mt:__call(...)
+    return M:new(...)
 end
 
-return setmetatable(tasklist, tasklist.mt)
+return setmetatable(M, M.mt)
