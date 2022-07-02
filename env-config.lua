e3fdebc40d24f46a7bca3870580dea459d3a9384
alex <alexandruturcanu915@gmail.com>
2022-07-02 23:26:04 +0300

    Added more enviroment options

--- env-config.lua
+++ env-config.lua
@@ -5,9 +5,11 @@
 --------------------------------------------------
 local setmetatable = setmetatable
 
+local awful = require "awful"
 local beautiful = require "beautiful"
 local gears = require "gears"
 
+local binaryTreeLayout = require "binary-tree-layout"
 local keybindingUtils = require "keybindings.utils"
 
 --------------------------------------------------
@@ -17,7 +19,8 @@ local keybindingUtils = require "keybindings.utils"
 ---@field editorCmd string #Full application call for editor.
 ---@field modKey string #Super modifiter.
 ---@field themePath string #Path to theme.lua file for beautiful setup.
----@field tags string[] #Names for tags to be used.
+---@field tags string[] #Collection of names to be used for the tags.
+---@field layouts table #Collection of layouts to be used.
 local env = { mt = {} }
 
 ---@return EnvConfig
@@ -29,6 +32,12 @@ function env:new()
     self.modKey = keybindingUtils.keys.super
 
     self.themePath = gears.filesystem.get_xdg_config_home() .. "awesome/theme/theme.lua"
+
+    self.tags = { "1", "2", "3", "4", "5", "6" }
+    self.layouts = {
+        binaryTreeLayout {},
+        awful.layout.suit.floating,
+    }
 
     beautiful.init(env.themePath)
 
