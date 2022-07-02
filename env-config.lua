22b2929fd4cf5579e069171ed05c30db91317c8f
alex <alexandruturcanu915@gmail.com>
2022-07-02 22:44:43 +0300

    Cleaned up env creator to be less funny

--- env-config.lua
+++ env-config.lua
@@ -8,6 +8,8 @@ local setmetatable = setmetatable
 local beautiful = require "beautiful"
 local gears = require "gears"
 
+local keybindingUtils = require "keybindings.utils"
+
 --------------------------------------------------
 ---@class EnvConfig
 ---@field terminal string #Name of terminal application
@@ -18,23 +20,15 @@ local gears = require "gears"
 ---@field tags string[] #Names for tags to be used.
 local env = { mt = {} }
 
----@class EnvArgs
----@field terminal? string #Name of the terminal application.
----@field editor? string #Name of editor.
----@field modKey? string #Super modifier to be used.
----@field themePath? string #Path to theme.lua file.
-
----@param args? EnvArgs #Optional arguments for environment configuration.
 ---@return EnvConfig
-function env:new(args)
-    args = args or {}
-    self.terminal = args.terminal or "kitty"
-    self.editor = args.editor or os.getenv "EDITOR" or "nvim"
+function env:new()
+    self.terminal = "kitty"
+    self.editor = os.getenv "EDITOR" or "nvim"
     self.editorCmd = self.terminal .. "-e" .. self.editor
 
-    self.modKey = args.modKey or "Mod4"
+    self.modKey = keybindingUtils.keys.super
 
-    self.themePath = args.themePath or gears.filesystem.get_xdg_config_home() .. "awesome/" .. "theme/theme.lua"
+    self.themePath = gears.filesystem.get_xdg_config_home() .. "awesome/theme/theme.lua"
 
     beautiful.init(env.themePath)
 
@@ -42,8 +36,8 @@ function env:new(args)
 end
 
 --------------------------------------------------
-function env.mt:__call(args)
-    return env:new(args)
+function env.mt:__call()
+    return env:new()
 end
 
 return setmetatable(env, env.mt)
