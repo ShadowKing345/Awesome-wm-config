4678c1093e401284f03bee76d86ccc66ec12278a
alex <alexandruturcanu915@gmail.com>
2022-07-02 23:25:15 +0300

    Cleaned up autostart config.

--- autostart-config.lua
+++ autostart-config.lua
@@ -1,37 +1,39 @@
+--[[
+
+    Autostart applications.
+    Does not deal with the autostart directory.
+
+--]]
+--------------------------------------------------
 local awful = require "awful"
 local gears = require "gears"
 
-local autostart = {}
+--------------------------------------------------
+local M = {
+    tempFile = "/tmp/awesomeWmFirstBoot",
+    mt = {}
+}
 
-function autostart.run()
-    if not gears.filesystem.file_readable "/tmp/awesomeWmFirstBoot" then
+---Automatically starts programs if the temp file is missing.
+---@param list string[] #Collection of applications to start.
+---@param tempFile? string #File for where tempfile will be created.
+function M.init(list, tempFile)
+    list = list or {}
+    tempFile = tempFile or M.tempFile
+
+    if not gears.filesystem.file_readable(tempFile) then
         -- Creating a blank file as a check.
-        awful.spawn.with_shell "touch /tmp/awesomeWmFirstBoot"
+        awful.spawn.with_shell("touch " .. tempFile)
 
-        -- environment
-
-        -- gnome environment
-        awful.spawn.with_shell "nvidia-settings --load-config-only" -- personal display config file. (recommened to use xrandar if you have no idea what it does)
-        awful.spawn.with_shell "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1"
-
-        -- utils
-        awful.spawn.with_shell "picom -b"
-        awful.spawn.with_shell "nm-applet"
-
-        -- apps
-        awful.spawn.with_shell "discord"
-        awful.spawn.with_shell "mailspring -b"
-    end
-end
-
-function autostart.run_from_file(file_)
-    local f = io.open(file_)
-    for line in f:lines() do
-        if line:sub(1, 1) ~= "#" then
-            awful.spawn.with_shell(line)
+        for _, item in ipairs(list) do
+            awful.spawn.with_shell(item)
         end
     end
-    f:close()
 end
 
-return autostart
+--------------------------------------------------
+function M.mt:__call(list)
+    return M.init(list)
+end
+
+return setmetatable(M, M.mt)
