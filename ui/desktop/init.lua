1b697fd333eedde93d2f305bf00f97391df7c8de
alex <alexandruturcanu915@gmail.com>
2022-08-05 14:04:32 +0300

    Fixed uniqueness issue.

--- ui/desktop/init.lua
+++ ui/desktop/init.lua
@@ -6,43 +6,60 @@
 --------------------------------------------------
 local awful     = require "awful"
 local beautiful = require "beautiful"
+local gTable    = require "gears.table"
 local wibox     = require "wibox"
 
 local clock = require "ui.desktop.clock"
 
 --------------------------------------------------
+---@type Desktop
 local M = { mt = {} }
 
----Creates a new wibox base for the desktop.
----@return any #The wibox.
-local function createWidget()
-    return wibox {
-        visible = true,
-        type    = "desktop",
-        bg      = beautiful.bg_normal .. "55",
+--- Creates a new instance of the wallpaper widget(?) and returns it.
+---@return unknown
+function M:createWallpaper(s)
+    local wallpaper = beautiful.wallpaper
+
+    if type(wallpaper) == "function" then
+        wallpaper = wallpaper()
+    end
+
+    local w = awful.wallpaper {
+        honor_workarea = true,
+        screen         = s,
+        widget         = {
+            bg         = beautiful["wallpaper_bg"] or beautiful.bg_normal,
+            halign     = "center",
+            valign     = "center",
+            image      = wallpaper,
+            stylesheet = beautiful["wallpaper_stylesheet"] or nil,
+            widget     = wibox.widget.imagebox,
+        },
     }
+
+    return w
 end
 
+--- Reloads all widgets and wallpaper of a desktop. If they do not exist it will create them instead.
+function M:reload()
+    if not self.wallpaper then
+        self.wallpaper = self:createWallpaper(self.screen)
+    end
+end
+
+--- Creates a new desktop manager.
+---@param env EnvConfig #Environment configs
+---@return any #Returns the
 function M:new(s, env)
-    local workarea = s.workarea
-    local widgets = {
-        clock = createWidget(),
-        sound = createWidget(),
+
+    local obj = {
+        env    = env or {},
+        screen = s,
     }
 
-    widgets.clock.x      = workarea.x + workarea.width / 2 - 200
-    widgets.clock.y      = workarea.y + 10
-    widgets.clock.width  = 400
-    widgets.clock.height = 100
-    widgets.clock.widget = clock(env)
+    gTable.crush(obj, M, true)
 
-    widgets.sound.x      = 10
-    widgets.sound.y      = workarea.height - 10 - 400
-    widgets.sound.width  = 400
-    widgets.sound.height = 400
-    widgets.sound.bg     = "#ff0000"
-
-    return widgets
+    return obj
 end
 
 --------------------------------------------------
@@ -51,3 +68,11 @@ function M.mt:__call(...)
 end
 
 return setmetatable(M, M.mt)
+
+--------------------------------------------------
+
+---@class Desktop #Desktop object used to manager the desktop for each screen.
+---@field env EnvConfig #Environment configurations set on setup.
+---@field mt any #Metadata table.
+---@field wallpaper any #Wallpaper widget.
+---@field screen any #Screen for desktop.
