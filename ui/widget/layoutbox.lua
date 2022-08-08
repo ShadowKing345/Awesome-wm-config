63d51bec22e643266f623387ae2675605c7512ae
alex <alexandruturcanu915@gmail.com>
2022-08-09 00:43:34 +0300

    Added creation of layoutlist wibox

--- ui/widget/layoutbox.lua
+++ ui/widget/layoutbox.lua
@@ -13,12 +13,45 @@ local layout    = require "awful.layout"
 local tooltip   = require "awful.tooltip"
 local beautiful = require "beautiful"
 
-local capi = { screen = screen, tag = tag }
+local layoutlist = require "ui.widget.layoutlist"
+
+local capi = { screen = screen, tag = tag, mouse = mouse }
 --------------------------------------------------
 local M    = {
     boxes = nil,
     mt    = {},
 }
+
+function M._createWibox(screen)
+    local w = wibox {
+        widget  = layoutlist { screen = screen },
+        width   = 150,
+        height  = #awful.layout.layouts * 24,
+        x       = 0,
+        y       = 0,
+        visible = false,
+        ontop   = true,
+    }
+
+    function w:_toggleWibox(geometry)
+        if self.visible then
+            self.visible = false
+        else
+            if geometry then
+                awful.placement.next_to(w, { prefered_position = "top", geometry = geometry })
+            end
+
+            self.visible = true
+        end
+    end
+
+    w:buttons(gTable.join(
+        awful.button({}, 4, function() layout.inc(-1, screen) end),
+        awful.button({}, 5, function() layout.inc(1, screen) end)
+    ))
+
+    return w
+end
 
 function M.getScreen(s)
     return s and capi.screen[s]
@@ -37,7 +70,7 @@ function M.defaultStyle(style)
     }, style or {})
 end
 
-function M.update(w, screen)
+function M.update(w, screen, tag)
     screen = M.getScreen(screen)
     local name = layout.getname(layout.get(screen))
     w._layoutboxToolTip:set_text(name or "[No Name]")
@@ -45,13 +78,14 @@ function M.update(w, screen)
     local img                                        = beautiful["layout_" .. name]
     w._button:get_children_by_id "imagebox"[1].image = img
     w._button:get_children_by_id "textbox"[1].text   = img and "" or name
+    if w._wibox then w._wibox.widget:updateSelected(tag or screen.selected_tag) end
 end
 
 function M.updateFromTag(t)
     local screen = M.getScreen(t.screen)
     local w = M.boxes[screen]
     if w then
-        M.update(w, screen)
+        M.update(w, screen, t)
     end
 end
 
@@ -101,8 +135,20 @@ function M:new(args)
             widget  = wibox.container.margin,
         }
 
+        if args.enableList then w._wibox = M._createWibox(screen) end
         w._layoutboxToolTip = tooltip { object = { w }, delay_show = 1 }
         w._button           = button
+
+        button:buttons(gTable.join(
+            awful.button({}, 1,
+                function() button.bg = style.bg.active end,
+                function() button.bg = style.bg.hover end
+            )
+        ))
+
+        if w._wibox then
+            button:connect_signal("button::press", function(_, _, _, _, _, geometry) w._wibox:_toggleWibox(geometry) end)
+        end
 
         if args.buttons then
             for _, b in ipairs(args.buttons) do
@@ -110,15 +156,6 @@ function M:new(args)
             end
         end
 
-        button:buttons(gTable.join(
-            awful.button({}, 1,
-                function()
-                    if args.layoutlist then args.layoutlist:toggle() end
-                    button.bg = style.bg.active
-                end,
-                function() button.bg = style.bg.hover end
-            )
-        ))
         button:connect_signal("mouse::enter", function() button.bg = style.bg.hover end)
         button:connect_signal("mouse::leave", function() button.bg = style.bg.normal end)
 
