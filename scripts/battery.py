b6aa39f24561d5436c68030b7151573dc4a517fd
alex <alexandruturcanu915@gmail.com>
2022-08-14 12:20:11 +0300

    Redid the script.

--- scripts/battery.py
+++ scripts/battery.py
@@ -1,38 +1,31 @@
 #!/bin/env python3
-from distutils.spawn import find_executable
-import sys, subprocess
+from shutil import which
+from subprocess import run, CompletedProcess
+import re
+from typing import Match
 
-acpi = "acpi"
+acpi: str = "acpi"
+command: str | None = which(acpi)
 
-command = find_executable(acpi)
+try:
+    if command is None:
+        raise Exception(f"Cannot find command {command}")
+    else:
+        result: CompletedProcess = run([command], capture_output=True, text=True)
+    
+        if result.returncode != 0 or len(result.stdout.strip()) == 0:
+            raise Exception(f"{command} return an error.")
+        else:
+            re_result: Match | None = re.search(r"(.*):([^,]*),([^,]*)[,]?(.*)?", result.stdout.strip())
+    
+            if not re_result:
+                raise Exception(f"Regex returned None.")
+            else:
+                res: list[str] = list(re_result.groups())
+                for k, v in enumerate(res):
+                    res[k] = v.strip().rstrip("%")
 
-if not command:
-    sys.exit(f"Cannot find {acpi}!")
+                print(",".join(res), end="")
+except Exception as _:
+    print("", end="")
 
-battery_info = subprocess.run(command, capture_output=True, text=True)
-
-if battery_info.stderr:
-    sys.exit(battery_info.stderr)
-
-split_string: list[str] = battery_info.stdout.split(",")
-
-name, state = split_string[0].split(":")
-
-name = name.strip()
-state = state.strip()
-
-percentage = split_string[1].strip().rstrip("%")
-
-time_left = split_string[2].strip() if len(split_string) == 3 else None
-
-match state:
-    case "Not charging":
-        state = "full"
-    case "Charging":
-        state = "charging"
-    case "Discharging":
-        state = "discharging"
-    case _:
-        state = "unknown"
-
-print(f"{name},{state},{percentage},{time_left if time_left else ''}")
