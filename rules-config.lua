bab85b249e32d3e9975b8849533ca229dcfc01aa
alex <alexandruturcanu915@gmail.com>
2022-08-18 19:14:00 +0300

    Reworked rules a bit.

--- rules-config.lua
+++ rules-config.lua
@@ -1,44 +1,60 @@
 local awful = require "awful"
-local beautiful = require "beautiful"
+local ruled = require "ruled"
 
-local rules = {}
+local M = {}
 
-rules.base_properties = {
-    border_width = beautiful.border_width,
-    border_color = beautiful.border_color,
-    focus = awful.client.focus.filter,
-    raise = true,
-    size_hints_honor = false,
-    screen = awful.screen.preferred,
-    placement = awful.placement.no_overlap + awful.placement.no_offscreen + awful.placement.centered,
-}
-
-rules.floating_any = {
-    instance = { "DTA", "copyq" },
-    class = { "Arandr", "Gpick", "Kruler", "MessageWin", "Sxiv", "Wpa_gui", "pinentry", "veromix", "xtightvncviewer" },
-    name = { "Event Tester" },
-    role = { "AlarmWindow", "pop-up" },
-}
-
-function rules:init(args)
+function M:init(args)
     args = args or {}
-    --self.base_properties.keys = args.hotkeys.keys.client
-    --self.base_properties.buttons = args.hotkeys.mouse.client
 
-    -- Build rules
-    self.rules = {
-        { rule = {}, properties = args.base_properties or self.base_properties },
-        { rule_any = args.floating_any or self.floating_any, properties = { floating = true } },
-        { rule_any = { type = { "normal", "dialog" } }, properties = { titlebars_enabled = true } }, -- discord
-        { rule = { class = "discord" }, properties = { tag = "3", screen = 1, maximized = true } },
+    ruled.client.connect_signal("request::rules", function()
+        ruled.client.append_rule {
+            id         = "global",
+            rule       = {},
+            properties = {
+                focus     = awful.client.focus.filter,
+                raise     = true,
+                screen    = awful.screen.preferred,
+                placement = awful.placement.no_overlap + awful.placement.no_offscreen
+            }
+        }
 
-        -- attempt to make the "splash screen" for Jetbrain IDE float. Shockingly worked.
-        { rule = { name = "win0" }, properties = { floating = true } },
-    }
+        ruled.client.append_rule {
+            id         = "floating",
+            rule_any   = {
+                instance = { "copyq", "pinentry" },
+                class    = {
+                    "Arandr", "Blueman-manager", "Gpick", "Kruler", "Sxiv",
+                    "Tor Browser", "Wpa_gui", "veromix", "xtightvncviewer"
+                },
+                name     = {
+                    "Event Tester",
+                },
+                role     = {
+                    "AlarmWindow",
+                    "ConfigManager",
+                    "pop-up",
+                }
+            },
+            properties = { floating = true }
+        }
 
-    self.rules.client = {}
+        ruled.client.append_rule {
+            id         = "titlebars",
+            rule_any   = { type = { "normal", "dialog" } },
+            properties = { titlebars_enabled = true }
+        }
 
-    awful.rules.rules = rules.rules
+        self.rules = {
+            { rule = {}, properties = args.base_properties or self.base_properties },
+            { rule_any = args.floating_any or self.floating_any, properties = { floating = true } },
+            { rule_any = { type = { "normal", "dialog" } }, properties = { titlebars_enabled = true } },
+            { rule = { class = "discord" }, properties = { tag = "3", screen = 1, maximized = true } }, -- discord
+        }
+
+        for _, v in pairs(args.rules or {}) do
+            table.insert(self.rules, v)
+        end
+    end)
 end
 
-return rules
+return M
