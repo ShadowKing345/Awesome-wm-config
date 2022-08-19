95741d30e0b011636aa34e50e4f72ca14d508997
alex <alexandruturcanu915@gmail.com>
2022-08-19 16:07:33 +0300

    Redid the error handleing

--- error-config.lua
+++ error-config.lua
@@ -1,32 +1,16 @@
---------------------------------------------------
---
---      Error configuration
---
+--[[
+
+      Error configuration
+
+]]
 --------------------------------------------------
 local naughty = require "naughty"
 
 --------------------------------------------------
-if awesome.startup_errors then
-    naughty.notify {
-        preset = naughty.config.presets.critical,
-        title = "Oops, there were errors during startup!",
-        text = awesome.startup_errors,
+naughty.connect_signal("request::display_error", function(message, startup)
+    naughty.notification {
+        urgency = "critical",
+        title   = "Oops, an error happened" .. (startup and " during startup!" or "!"),
+        message = message
     }
-end
-
-do
-    local in_error = false
-    awesome.connect_signal("debug::error", function(err)
-        if in_error then
-            return
-        end
-        in_error = true
-
-        naughty.notify {
-            preset = naughty.config.presets.critical,
-            title = "Oops, an error happened!",
-            text = tostring(err),
-        }
-        in_error = false
-    end)
-end
+end)
