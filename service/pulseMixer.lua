c8bdc923f27b06b513bd8ef91d7f891dc58b30d8
alex <alexandruturcanu915@gmail.com>
2022-08-31 15:38:07 +0300

    Reworking pulse mixer service

--- service/pulseMixer.lua
+++ service/pulseMixer.lua
@@ -1,36 +1,26 @@
---------------------------------------------------
---
---      Pulse Mixer API mapper
---
---------------------------------------------------
+--[[
 
--- Features
--- [*] Toggle mute, mute, unmute.
--- [*] Get mute state.
--- [*] Change volume by delta amount.
--- [*] Get / Set volume.
--- [*] Set max volume.
--- [x] Select default / active.
--- [*] Get source/sink information.
+      Pulse Mixer API mapper
 
+]]
 --------------------------------------------------
-local setmetatable = setmetatable
-
 local awful   = require "awful"
 local gTable  = require "gears.table"
-local gString = require "gears.string"
+local naughty = require "naughty"
+
+local utils = require "utils"
 
 --------------------------------------------------
----@class PulseMixer
----@field sources SourceSink[] #Collection of sources.
----@field sinks SourceSink[] #Collection of sources.
-local pulseMixer = {
-    mt             = {},
-    ---@type SourceSink[]
-    sources        = {},
-    ---@type PulseCommands
-    commands       = {
+---Service for querying and controlling PulseAudio using pulsemixer commands.
+---@class PulseMixerService
+---@field objects PulseObject[] #Collection of pulse objects.
+---@field types PulseTypes #Tells the service what is the name of an output.
+local M = {
+    mt       = {},
+    objects  = {},
+    commands = {
         cmd          = "pulsemixer",
+        list         = "-l",
         id           = "--id %s",
         getMuted     = "--get-mute",
         mute         = "--mute",
@@ -42,380 +32,186 @@ local pulseMixer = {
         setVolumeAll = "--set-volume-all ",
         maxVolume    = "--max-volume %d",
     },
-    commandFormat  = "%s %s %s",
-    active         = {
-        sink   = nil,
-        source = nil,
+    types    = {
+        input = "Source",
+        output = "Sink",
     },
-    ---@type SourceSinkType
-    sourceSinkType = {}
 }
 
 ---Changes the volume of a source/sink by a delta amount.
 ---@param amount number #The amount to change by.
 ---@param id? string #Id for source/sink.
-function pulseMixer:changeVolume(amount, id)
-    id = id or self.active.sink
-
-    awful.spawn.easy_async(self.commandFormat:format(
-        self.commands.cmd,
-        self:_getId(id),
-        self.commands.changeVolume:format(amount)
-    ), function(_, _, _, exitcode)
-        if exitcode ~= 0 then
-            return
-        end
-
-        local sourceSink = self:_getSourceSinkById(id)
-        if sourceSink then
-            for k, v in ipairs(sourceSink.volumes) do
-                sourceSink.volumes[k] = v + amount
-            end
-        end
-    end)
+function M:changeVolume(amount, id)
 end
 
 ---Sets the volume to a given amount.
 ---Having an array of ammounts will change for each unique channel in order.
 ---@param amounts number|number[] #The ammount/amounts to change for each volume channel.
 ---@param id string #Id for source/sink
-function pulseMixer:setVolume(amounts, id)
-    local cmd
-    local t = type(amounts)
-
-    if t == "table" then
-        cmd = self.commands.setVolumeAll .. table.concat(amounts, ":")
-    elseif t == "number" then
-        cmd = self.commands.setVolume:format(amounts)
-    end
-
-    awful.spawn.easy_async(
-        self.commandFormat:format(
-            self.commands.cmd,
-            self:_getId(id),
-            cmd
-        ),
-        function(_, _, _, exitcode)
-            if exitcode ~= 0 then
-                return
-            end
-
-            local sourceSink = self:_getSourceSinkById(id)
-
-            if t == "table" then
-                for k, v in ipairs(amounts) do
-                    sourceSink.volumes[k] = v
-                end
-            elseif t == "number" then
-                for k = 1, #sourceSink.volumes, 1 do
-                    sourceSink.volumes[k] = amounts
-                end
-            end
-        end
-    )
+function M:setVolume(amounts, id)
 end
 
 ---Gets the current volumes for all channels.
 ---Returns an empty array if none was found.
 ---@param id string #Id for source/sink.
 ---@return number[]
-function pulseMixer:getVolume(id)
-    id = id or self.active.sink
-    local sourceSink = self:_getSourceSinkById(id)
-
-    if sourceSink then
-        return sourceSink.volumes
-    end
-
-    local cmd = io.popen(self.commandFormat:format(self.commands.cmd, self:_getId(id), self.commands.getVolume))
-    local stdOut, result, cleanExit = cmd:read "a", {}, cmd:close()
-
-    if cleanExit then
-        for channel in stdOut:gmatch "%d+" do
-            table.insert(result, tonumber(channel))
-        end
-    end
-
-    return result
+function M:getVolume(id)
 end
 
 ---Gets the current muted state
 ---@param id? string #Id of source or sink.
 ---@return boolean|nil
-function pulseMixer:getMuted(id)
-    id = id or self.active.sink
-    local sourceSink = self:_getSourceSinkById(id)
-
-    if sourceSink then
-        return sourceSink.is_muted
-    end
-
-    local cmd = io.popen(self.commandFormat:format(self.commands.cmd, self:_getId(id), self.commands.getMuted))
-    local stdOut, cleanExit = cmd:read "a", cmd:close()
-
-    if cleanExit then
-        return stdOut == "0"
-    end
-
-    return nil
+function M:muted(id)
 end
 
 ---Mutes a source or sink
 ---@param id? string #Id of source or sink
-function pulseMixer:mute(id)
-    id = id or self.active.sink
-
-    awful.spawn.easy_async(
-        self.commandFormat:format(
-            self.commands.cmd,
-            self:_getId(id),
-            self.commands.mute
-        ),
-        function(_, _, _, exitcode)
-            if exitcode ~= 0 then
-                return
-            end
-            local sourceSink = self:_getSourceSinkById(id)
-            if sourceSink then
-                sourceSink.is_muted = true
-            end
-        end
-    )
+function M:mute(id)
 end
 
 ---Unmutes a source or sink
 ---@param id? string #Id of source or sink
-function pulseMixer:unmute(id)
-    id = id or self.active.sink
-
-    awful.spawn.easy_async(
-        self.commandFormat:format(
-            self.commands.cmd,
-            self:_getId(id),
-            self.commands.unmute
-        ),
-        function(_, _, _, exitcode)
-            if exitcode ~= 0 then
-                return
-            end
-            local sourceSink = self:_getSourceSinkById(id)
-            if sourceSink then
-                sourceSink.is_muted = false
-            end
-        end
-    )
+function M:unmute(id)
 end
 
 ---Toggles mutes a source or sink
 ---@param id? string #Id of source or sink
-function pulseMixer:toggleMute(id)
-    id = id or self.active.sink
-
-    awful.spawn.easy_async(
-        self.commandFormat:format(
-            self.commands.cmd,
-            self:_getId(id),
-            self.commands.toggleMute
-        ),
-        function(_, _, _, exitcode)
-            if exitcode ~= 0 then
-                return
-            end
-            local sourceSink = self:_getSourceSinkById(id)
-            if sourceSink then
-                sourceSink.is_muted = not sourceSink.is_muted
-            end
-        end
-    )
+function M:toggleMute(id)
 end
 
 ---Sets the default sink/source used.
 ---@param id string #Id of source/sink
----@param type SourceSinkType #The type to set.
-function pulseMixer:setDefault(id, type)
-    if not id then
-        return
-    end
-
-    if type == self.sourceSinkType.SINK then
-        self.active.sink = id
-    end
-
-    if type == self.sourceSinkType.SOURCE then
-        self.active.source = id
-    end
-end
-
----Gets the information of a source/sink. Returns the default sink if no id was provided.
----@param id string #Id for source/sink
----@return SourceSink|nil
-function pulseMixer:getInfo(id)
-    id = id or self.active.sink
-    local sourceSink = self:_getSourceSinkById(id)
-
-    if sourceSink then
-        return sourceSink
-    end
-
-    local cmd = io.popen(self.commandFormat:format(self.commands.cmd, "-l", ""))
-    local stdOut, cleanExit = cmd:read "a", cmd:close()
-
-    if cleanExit then
-        for str in stdOut:gmatch "[^\n]+" do
-            local obj = self:_parseStringIntoSourceSink(str)
-            if obj.id == id then
-                return obj
-            end
-        end
-    end
-
-    return nil
+function M:setDefault(id)
 end
 
 ---Sets the maximum volume.
 ---Note this is just a wrapper method for the flag  of the same name. I have no idea what it is doing behind the scenes.
 ---@param amount number #The volume level to be set.
 ---@param id string #Id for source/sink
-function pulseMixer:setMaxVolume(amount, id)
-    id = id or self.active.sink
-
-    awful.spawn(self.commandFormat:format(
-        self.commands.cmd,
-        self:_getId(id),
-        self.commands.maxVolume:format(amount)
-    ))
+function M:setMaxVolume(amount, id)
 end
 
 ---Gets the or an empty string.
 ---@param id? string
 ---@return string
-function pulseMixer:_getId(id)
-    if id then
-        return self.commands.id:format(id)
-    elseif self.active.sink then
-        return self.commands.id:format(self.active.sink)
-    end
-    return ""
+function M:_getId(id)
 end
 
 ---Returns a source or sink if one with matching id is found. nil otherwise
 ---@param id string #Id for source or sink.
----@return SourceSink|nil
-function pulseMixer:_getSourceSinkById(id)
-    for _, source in pairs(self.sources) do
-        if source.id == id then
-            return source
-        end
-    end
-
-    return nil
+---@return PulseObject | nil
+function M:_getObjectById(id)
 end
 
 ---Parses a string into a SourceSink object.
 ---@param str string
----@return SourceSink
-function pulseMixer:_parseStringIntoSourceSink(str)
-    local t, data = str:match "(%w+):%s+([^.]+).?"
+---@return PulseObject | nil
+function M:_parseObject(str)
+    local re = "(.*):%s+ID: ([^,]+), Name: ([^,]+), Mute: ([^,]+), Channels: ([^,]+), Volumes: %[(.*)%](.*)"
 
+    local type, id, name, muted, channels, volume, default = str:match(re)
+
+    if not type then
+        return
+    end
+
+    local volumes = {}
+    for i in volume:gmatch "[^,]+" do
+        local v = tonumber((utils.trim(utils.trim(i), "'"):gsub("(.*)%%", "%1")))
+        table.insert(volumes, v)
+    end
+
+
+    ---@type PulseObject
     return {
-        type       = self.sourceSinkType[t],
-        id         = data:match "ID: ([^,]+),.*",
-        name       = data:match "Name: ([^,]+),.*",
-        is_muted   = data:match "Mute: ([^,]+),.*" == "1",
-        channels   = tonumber(data:match "Channels: ([^,]+),.*"),
-        volumes    = (function()
-            local result = {}
-
-            for _, i in pairs(gString.split(data:match "Volumes: %[(.*)%]", ",")) do
-                table.insert(result, tonumber(i:match "^%s*(.-)%s*$":match "%d+"))
-            end
-            return result
-        end)(),
-        is_default = data:match ".*,%s+(.*)" == "Default"
+        id       = id,
+        name     = name,
+        channels = tonumber(channels),
+        volumes  = volumes,
+        output   = type:match "[^%s]+" == self.types.output,
+        muted    = muted == "1",
+        default  = default:match "Default" ~= nil,
     }
 end
 
 ---Initalize sources and sinks.
----@param args PulseMixerServiceArgs #Arguments.
----@param results? fun(result: SourceSink[]):nil  #Callback called when the operation has finished.
-function pulseMixer:initSourceSinks(args, results)
-    awful.spawn.easy_async_with_shell(self.commandFormat:format(self.commands.cmd, "-l", ""),
-        function(stdOut, _, _, exitcode)
-            if exitcode ~= 0 then
-                return
+function M:init()
+
+    awful.spawn.easy_async_with_shell(("%s %s %s"):format(self.commands.cmd, self.commands.list, ""),
+        function(stdout, stderror, _, exitCode)
+            if exitCode ~= 0 then
+                naughty.notification {
+                    title = "PulseMixerService Error",
+                    text = "The pulsemixer service was not able to initalize. Reason:\n" .. stderror,
+                    urgency = "critical",
+                }
             end
 
-            for str in stdOut:gmatch "[^\n]+" do
-                local obj = self:_parseStringIntoSourceSink(str)
-
-                if obj.type then
-                    table.insert(self.sources, obj)
-                    if obj.is_default then
-                        self.active[(obj.type == self.sourceSinkType.SINK) and "sink" or "source"] = obj.id
-                    end
+            self.objects = {}
+            for i in stdout:gmatch "[^\r\n]+" do
+                local object = self:_parseObject(i)
+                if object then
+                    table.insert(self.objects, object)
                 end
             end
-
-            if args.sink then
-                self.active.sink = args.sink
-            end
-
-            if args.source then
-                self.active.source = args.source
-            end
-
-            if results then results(self.sources) end
-        end)
+        end
+    )
 end
 
----Creates a new pulseMixer service.
+---Creates a new pulseMixer service instance.
 ---@param args PulseMixerServiceArgs
----@return PulseMixer
-function pulseMixer:new(args)
+---@return PulseMixerService
+function M:new(args)
     args = args or {}
 
-    self.commands = gTable.merge(self.commands, args.commands or {})
+    if args.commands then
+        self.commands = gTable.merge(self.commands, args.commands)
+    end
 
-    self.sourceSinkType = {}
-    self.sourceSinkType.SOURCE = args.sourceType or "Source"
-    self.sourceSinkType[self.sourceSinkType.SOURCE] = "SOURCE"
-    self.sourceSinkType.SINK = args.sinkType or "Sink"
-    self.sourceSinkType[self.sourceSinkType.SINK] = "SINK"
+    if args.types then
+        self.types = gTable.merge(self.types, args.types)
+    end
 
-    self:initSourceSinks(args)
+    self:init()
     return self
 end
 
 --------------------------------------------------
-function pulseMixer.mt:__call(...)
-    return pulseMixer:new(...)
+---Creates a new pulseMixer service instance.
+---@param args PulseMixerServiceArgs
+---@return PulseMixerService
+function M.mt:__call(args)
+    return M:new(args)
 end
 
-return setmetatable(pulseMixer, pulseMixer.mt)
+return setmetatable(M, M.mt)
 --------------------------------------------------
 
----@class SourceSink #Object mapping of the name.
----@field type SourceSinkType #Variable to tell the difference between a source or a sink.
+---The input output type names for PulseAudio.
+---Note we dont care about sink input or source output just if it's a input or output.
+---@class PulseTypes
+---@field output string #The output name. (Default: Sink)
+---@field input string #The input name. (Default: Source)
+
+---Defines a PulseAudioObject.
+---@class PulseObject
 ---@field id string #Id of source/sink.
 ---@field name string #Name of source/sink.
----@field is_muted boolean #Is source/sink muted.
+---@field muted boolean #Is source/sink muted.
 ---@field channels number #Number of channels.
 ---@field volumes number[] #Collection of all the channel volumes.
----@field is_default boolean #Is source/sink the default.
+---@field output boolean #Says if this is an output.
+---@field default boolean #Is source/sink the default.
 
----@class SourceSinkType #Enum of type.
----@field SOURCE string
----@field SINK string
-
----@class PulseMixerServiceArgs #Arguments for pulsemixer service.
+---Arguments for pulsemixer service.
+---@class PulseMixerServiceArgs
 ---@field commands PulseCommands #List of commands prefixes. Used to overwrite the default ones.
----@field sink string #Default sink id to be used.
----@field source string #Default source id to be used.
----@field sourceType string #Name of the source type.
----@field sinkType string #Name of the sink type.
+---@field types PulseTypes #What are the pulse types. Use this to overwrite the default names.
 
----@class PulseCommands #Commands used by the pulsemixer service in order to call for information.
+---Commands for the pulsemixer bash command.
+---@class PulseCommands
 ---@field cmd string #Main command *default "pulsemixer"*
+---@field list string #Lists all the sources and sinks.
 ---@field id string #Flag for id that accepts a string *default "--id %s"*
 ---@field getMute string #Flag for getting the muted state *default "--get-mute"*
 ---@field mute string #Flag to mute *default "--mute"*
