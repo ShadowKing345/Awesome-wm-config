6a8f57cf92fc10758a5e553b99fd9dbf68eccdcd
alex <alexandruturcanu915@gmail.com>
2022-09-09 15:08:21 +0300

    altered.

--- modules/pulseaudio/test.lua
+++ modules/pulseaudio/test.lua
@@ -1,10 +1,10 @@
 #!/bin/env lua
 --This is a visual test not a automatic test.
-package.cpath = "../init.so;./init.so" .. package.cpath
-local inspect = require "pl.import_into"().pretty.write
+package.cpath = "../init.so;./init.so;" .. package.cpath
+local inspect = require "pl.import_into" ().pretty.write
 local pulseaudio = require "pulseaudio"
 
 print(inspect(pulseaudio))
-print(inspect(pulseaudio.get_object { type = "sink", index = 50 }))
-print(inspect(pulseaudio.set_volume { type = "sink", index = 49, volume = 32768 }))
-print(inspect(pulseaudio.mute_object { type = "sink", index = 49, mute = false }))
+print(inspect(pulseaudio.get_object {}))
+print(inspect(pulseaudio.set_volume { type = "sink", index = 174, volume = 655 }))
+-- print(inspect(pulseaudio.mute_object { type = "sink", index = 49, mute = false }))
