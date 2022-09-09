39b563883010e81fdc49f3c69a0edcdb3624f177
alex <alexandruturcanu915@gmail.com>
2022-09-09 17:51:10 +0300

    Added a pcall to not fail on inital load.

--- modules/init.lua
+++ modules/init.lua
@@ -1,3 +1,11 @@
-return {
-    pulseaudio = require "pulseaudio" or nil,
+local M = {
+    pulseaudio = nil,
 }
+
+local t_pulseaudio, pulseaudio = pcall(require, "pulseaudio")
+
+if t_pulseaudio then
+    M.pulseaudio = pulseaudio
+end
+
+return M
